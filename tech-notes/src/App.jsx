85ba61ba9cbe6562e2a75b4f334595bb68d230e0
John Q. Public <jtsdev2023@gmail.com>
2024-01-06 11:34:24 -0500

    00:25:07 - adding items to todo list works when pressing <enter> key but not when clicking the [add] button.

--- tech-notes/src/App.jsx
+++ tech-notes/src/App.jsx
@@ -2,41 +2,58 @@ import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [newItem, setNewItem] = useState("")
+  const [myNewItem, setMyNewItem] = useState("");
+  const [myTodos, setMyTodos] = useState([]);
+
+  function myHandleSubmit(myTestEvent) {
+    myTestEvent.preventDefault();
+
+    setMyTodos((myCurrentTodos) => {
+      return [
+        ...myCurrentTodos,
+        {
+          id: crypto.randomUUID(),
+          title: myNewItem,
+          completed: false,
+        },
+      ];
+    });
+  }
 
   return (
     <main>
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input
-          value={newItem}
-          type="text" name="item"
-          id="item"
-          placeholder="enter a new item..."
-          onChange={myTestEvent => setNewItem(myTestEvent.target.value)}
-        />
-        <button className="btn" type="button">Add</button>
-      </div>
-    </form>
-    <h1 className="header">Todo List</h1>
-    <ul className="list">
-      <li>
-        <label htmlFor="">
-          <input type="checkbox" name="" id="" />
-          Item 1
-        </label>
-        <button className="btn btn-danger" type="button">Delete</button>
-      </li>
-      <li>
-        <label htmlFor="">
-          <input type="checkbox" name="" id="" />
-          Item 2
-          </label>
-          <button className="btn btn-danger" type="button">Delete</button>
-      </li>
-    </ul>
+      <form onSubmit={myHandleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            value={myNewItem}
+            type="text"
+            name="item"
+            id="item"
+            placeholder="enter a new item..."
+            onChange={(myTestEvent) => setMyNewItem(myTestEvent.target.value)}
+          />
+          <button className="btn" type="button">
+            Add
+          </button>
+        </div>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        {myTodos.map((todo) => {
+          return (
+            <li>
+              <label htmlFor="">
+                <input type="checkbox" name="" id="" />
+                Item 1
+              </label>
+              <button className="btn btn-danger" type="button">
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </main>
   );
 }
- 
